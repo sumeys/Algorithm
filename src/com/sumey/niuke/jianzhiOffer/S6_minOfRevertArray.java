894ecc81bcb009a4b842e9dde14bc92c78a25501
sumeys <sumeys@163.com>
2018-09-08 17:22:53 +0800

    求旋转数组最小值

---
+++ src/com/sumey/niuke/jianzhiOffer/S6_minOfRevertArray.java
@@ -0,0 +1,28 @@
+package com.sumey.niuke.jianzhiOffer;
+
+/**
+ * @author sumey
+ * @date 2018/9/8 下午5:14
+ */
+
+/**
+ * 题目描述：把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+ * 输入一个非减排序的数组的一个旋转，输出旋转数组的最小元素。
+ * 例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。 NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。
+ */
+
+//这个题目还是挺简单的，就算数组中存在相等的元素也没有关系，这样依然能找到最小的元素
+public class S6_minOfRevertArray {
+    public int minNumberInRotateArray(int[] array) {
+        if (0 == array.length) {
+            return 0;
+        }
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                return array[i + 1];
+            }
+        }
+        return array[0];
+    }
+
+}
